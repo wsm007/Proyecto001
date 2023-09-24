6d2b1d5a685cb5bdab9c8a96f36107d11f780906
Wilbert Santos <9063917+wsm007@users.noreply.github.com>
2023-09-23 21:47:52 -0500

    Eliminación de la libreria @hapi/boom

--- src/middlewares/error.handler.js
+++ src/middlewares/error.handler.js
@@ -13,19 +13,3 @@ export function errorHandler (err, req, res, next) {
     stack: err.stack
   });
 };
-
-// No funcionó
-// // Proprociona un formato para reportar los errores de Boom
-// export function boomErrorHandler (err, req, res, next) {
-//   console.info('3. errorBoomHandler');
-//   if (err.isBoom){
-
-//     const{ output } = err;
-//     res.status(output.statusCode).json(output.payload);
-//   } else {
-//     next(err);
-//   }
-// }
-
-
-
